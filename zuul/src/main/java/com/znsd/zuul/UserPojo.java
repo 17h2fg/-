366f1eb754cd6029a377d0f5ce470b6731ba4a44
wuwenlongs <2831425419@qq.com>
2019-02-11 15:39:24 +0800

    lalala

---
+++ zuul/src/main/java/com/znsd/zuul/UserPojo.java
@@ -0,0 +1,13 @@
+package com.znsd.zuul;
+
+import java.io.Serializable;
+
+/**
+ * 作者：伍文龙
+ * 日期：2019年2月11日
+ */
+public class UserPojo implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+}
